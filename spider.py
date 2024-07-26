f3c2cc78383fe22ad13398254512152b73059666
HellReys <wallpapertopia@gmail.com>
2024-07-26 17:53:32 -0400

    Version 1

---
+++ spider.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import requests
+import re
+import urllib.parse
+
+target_url = "http://10.0.2.5/mutillidae/"
+target_links = []
+
+def extract_links_from(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', response.content.decode(errors="ignore"))
+
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        link = urllib.parse.urljoin(url, link)
+
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
